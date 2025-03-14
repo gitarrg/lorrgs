a06020529299d69320351ba80502b98cb1eef62a
arrg <arrg@lorrgs.io>
2025-03-14 11:33:32 +0000

    Feral: Add Tiger's Fury

--- lorgs/data/classes/druid.py
+++ lorgs/data/classes/druid.py
@@ -69,10 +69,11 @@ DRUID_BALANCE.add_spell(     spell_id=202770, cooldown=60,  duration=8,  color="
 
 
 DRUID_FERAL.add_spell(       spell_id=106951, cooldown=120, duration=15,                  name="Berserk",                        icon="ability_druid_berserk.jpg", variations=[102543], tags=[SpellTag.DAMAGE])
-DRUID_FERAL.add_spell(       spell_id=58984,  cooldown=120,              color="#999999", name="Shadowmeld ",                    icon="ability_ambush.jpg",                        show=False)
-DRUID_FERAL.add_spell(       spell_id=108291, cooldown=300, duration=45, color="#fcdf03", name="Hearth of the Wild ",            icon="spell_holy_blessingofagility.jpg")
-DRUID_FERAL.add_buff(        spell_id=197625,                            color="#11cff5", name="Moonkin Form ",                  icon="spell_nature_forceofnature.jpg")
-DRUID_FERAL.add_buff(        spell_id=274837, cooldown=45,  duration=6,  color="#c43333", name="Feral Frenzy ",                  icon="ability_druid_rake.jpg", show=False)
+DRUID_FERAL.add_spell(       spell_id=58984,  cooldown=120,              color="#999999", name="Shadowmeld",                     icon="ability_ambush.jpg",                        show=False)
+DRUID_FERAL.add_spell(       spell_id=108291, cooldown=300, duration=45, color="#fcdf03", name="Hearth of the Wild",             icon="spell_holy_blessingofagility.jpg")
+DRUID_FERAL.add_buff(        spell_id=197625,                            color="#11cff5", name="Moonkin Form",                   icon="spell_nature_forceofnature.jpg")
+DRUID_FERAL.add_buff(        spell_id=274837, cooldown=45,  duration=6,  color="#c43333", name="Feral Frenzy",                   icon="ability_druid_rake.jpg", show=False)
+DRUID_FERAL.add_buff(        spell_id=5217,   cooldown=30,  duration=6,  color="#fcdf03", name="Tiger's Fury",                   icon="ability_mount_jungletiger.jpg", show=False)
 
 
 DRUID_GUARDIAN.add_spell(    spell_id=108292, cooldown=300, duration=45, color="#fcdf03", name="Heart of the Wild",              icon="spell_holy_blessingofagility.jpg",          show=False, tags=[SpellTag.TANK])
