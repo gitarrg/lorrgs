1c11a001b670bea6f02b8d8a8c1a8f39c15db7a5
arrg <arrg@lorrgs.io>
2025-03-14 13:01:37 +0000

    add Gallywix Spells

--- lorgs/data/expansions/the_war_within/raids/undermine/gallywix.py
+++ lorgs/data/expansions/the_war_within/raids/undermine/gallywix.py
@@ -47,3 +47,103 @@ EYE_OF_KEZAN = GALLYWIX.add_trinket(
 > grant you 284 <Primary Stat> up to 20 times, decaying rapidly upon leaving combat.
 > While fully empowered, the Eye instead deals 64528 Fire damage to enemies or heals allies for 96796.
 """
+
+################################################################################
+# Spells
+
+
+########################################
+# P1
+
+# Frontals
+boss.add_cast(
+    spell_id=466340,
+    name="Scatterblast Canisters",
+    duration=3,
+    cooldown=30,
+    color="hsl(0, 50%, 50%)",
+    icon="spell_mage_infernoblast.jpg",
+)
+
+
+boss.add_cast(
+    spell_id=465952,
+    name="Big Bad Buncha Bombs",
+    duration=3,
+    cooldown=4.5,
+    color="hsl(30, 60%, 50%)",
+    icon="ships_ability_bombers.jpg",
+)
+
+# Raid AoE
+boss.add_cast(
+    spell_id=466751,
+    name="Venting Heat",
+    duration=1,
+    cooldown=4,
+    color="hsl(20, 60%, 50%)",
+    icon="ability_warlock_inferno.jpg",
+    show=False,
+)
+
+
+########################################
+# P2
+
+
+# Canisters
+boss.add_cast(
+    spell_id=466342,
+    name="Tick-Tock Canisters",
+    duration=3,
+    color="hsl(0, 50%, 50%)",
+    icon="inv_eng_bombstonestun.jpg",
+)
+
+
+boss.add_buff(
+    spell_id=469293,
+    name="Giga Coils",
+    color="hsl(200, 60%, 50%)",
+    icon="inv_10_engineering_manufacturedparts_electricalparts_color1.jpg",
+)
+
+
+boss.add_buff(
+    spell_id=1214590,
+    name="TOTAL DESTRUCTION!!!",
+    color="hsl(120, 50%, 50%)",
+    icon="ui_majorfactions_rocket.jpg",
+)
+
+
+# Tank Hit
+# Not sure if we'd pref to track the debuff (467064) on the tanks
+boss.add_cast(
+    spell_id=466958,
+    name="Molten Gold Knuckles",
+    duration=1.5,
+    cooldown=22,
+    icon="inv_misc_desecrated_leatherglove.jpg",
+    color="hsl(220, 80%, 70%)",
+    show=False,
+)
+
+
+################################################################################
+# Phases
+
+boss.add_phase(name="P2 (1)", spell_id=1216846, event_type="applybuff", count=2)  # Holding a Wrench
+boss.add_phase(name="P2 (2)", spell_id=1216846, event_type="applybuff", count=4)  # Holding a Wrench
+boss.add_phase(name="Int", spell_id=1214369, event_type="cast")  # TOTAL DESTRUCTION!!!
+boss.add_phase(name="P3 (1)", spell_id=1214590, event_type="removebuff")  # TOTAL DESTRUCTION!!!
+boss.add_phase(name="P3 (2)", spell_id=466342, event_type="cast", count=2)
+boss.add_phase(name="P3 (3)", spell_id=1223658, event_type="cast", count=3)
+
+
+# boss.add_phase(name="P2 (1)", spell_id=469387, event_type="applybuff")  # Carrier Giga Bomb
+# boss.add_phase(name="P2 (3)", spell_id=1216846, event_type="applybuff", count=2)
+# boss.add_phase(name="Int", spell_id=1214229, event_type="applybuff")  # Armageddon-class Plating
+# boss.add_phase(name="P3 (2)", spell_id=466342, event_type="begincast", count=2)  # Tick-Tock Canisters
+# boss.add_phase(name="P3 (3)", spell_id=1223658, event_type="cast", count=3)  # Suppression
+# boss.add_phase(name="P3 (4)", spell_id=466342, event_type="begincast", count=4)  # Tick-Tock Canisters
