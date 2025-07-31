ec535018b1c20d1da01f888bb7ae053e5019e9ce
arrg <arrg@lorrgs.io>
2025-07-31 19:08:25 +0100

    add Naazindhri

--- lorgs/data/expansions/the_war_within/raids/manaforge_omaga/naazindhri.py
+++ lorgs/data/expansions/the_war_within/raids/manaforge_omaga/naazindhri.py
@@ -1,7 +1,7 @@
-"""02: Soulbinder Naazindhri
+"""03: Soulbinder Naazindhri
 
 
->>> scripts/load_report.py "https://www.warcraftlogs.com/reports/DzLQG1Rkp4xtdJgh?fight=16"
+>>> scripts/load_report.py "https://www.warcraftlogs.com/reports/AvbNVBmyrKPDCT4L?fight=17"
 
 """
 
@@ -36,3 +36,46 @@ SOULBINDERS_EMBRACE = WowTrinket(
 > reduce your damage taken by 50% for 20 sec or until 2418789 damage has been prevented. (1 Min Cooldown)
 """
 SOULBINDERS_EMBRACE.add_specs(*TANK.specs)
+
+
+################################################################################
+# Spells
+
+
+# Orbs
+boss.add_cast(
+    spell_id=1227276,
+    name="Soulfray Annihilation",
+    duration=6,
+    color="hsl(30, 60%, 50%)",
+    icon="spell_arcane_arcane03.jpg",
+)
+
+# TODO: Lines?
+
+
+# "Arcane Expulsion" = Knockback + drop stuff
+boss.add_cast(
+    spell_id=1242088,
+    name="Arcane Expulsion",
+    duration=4,
+    cooldown=15,
+    color="hsl(0, 60%, 60%)",
+    icon="spell_nature_astralrecalgroup.jpg",
+)
+
+
+# new cages spawn
+boss.add_cast(
+    spell_id=1225582,
+    name="Soul Calling",
+    duration=3,
+    color="hsl(30, 60%, 50%)",
+    icon="ability_racial_etherealconnection.jpg",
+)
+
+
+################################################################################
+# Phases
+
+boss.add_phase(name="P{count}", spell_id=1225582, event_type="cast")
