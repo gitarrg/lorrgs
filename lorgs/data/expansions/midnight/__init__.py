4393bcdb06a1163d2e349e762916a610e37d384d
arrg <arrg@lorrgs.io>
2026-06-18 19:27:09 +0100

    add Sporefall

--- lorgs/data/expansions/midnight/__init__.py
+++ lorgs/data/expansions/midnight/__init__.py
@@ -1,20 +1,18 @@
 # Dungeons
 
 # Raids
-from .raids import VOIDSPIRE
-from .raids import DREAMRIFT
-from .raids import MARCH_ON_QUALDANAS
-
 from .consumables import MIDNIGHT_CONSUMABLES
+from .raids import DREAMRIFT, MARCH_ON_QUALDANAS, SPOREFALL, VOIDSPIRE
 
 # Seasons
 from .seasons import MIDNIGHT_SEASON1
 
 
 __all__ = [
-    "VOIDSPIRE",
     "DREAMRIFT",
     "MARCH_ON_QUALDANAS",
-    "MIDNIGHT_SEASON1",
     "MIDNIGHT_CONSUMABLES",
+    "MIDNIGHT_SEASON1",
+    "SPOREFALL",
+    "VOIDSPIRE",
 ]
